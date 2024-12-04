29b5ad173b18dbcf36e9739a0ee061de40023ef3
Adnan <adbak.dev@gmail.com>
2024-12-04 17:36:32 +0100

    sockets

--- websockets/exercise-socketio/frontend/socketio-chat.js
+++ websockets/exercise-socketio/frontend/socketio-chat.js
@@ -5,11 +5,16 @@ const msgs = document.getElementById("msgs");
 const presence = document.getElementById("presence-indicator");
 let allChat = [];
 
-/*
- *
- * Code goes here
- *
- */
+const socket = io("http://localhost:8080");
+socket.on("connect", () => {
+  console.log("connected");
+  presence.innerText = "🟢";
+});
+
+socket.on("disconnect", () => {
+  console.log("disconnected");
+  presence.innerText = "🔴";
+});
 
 chat.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -17,12 +22,14 @@ chat.addEventListener("submit", function (e) {
   chat.elements.text.value = "";
 });
 
+socket.on("msg:get", (data) => {
+  allChat = data.msg;
+  render();
+});
+
 async function postNewMsg(user, text) {
-  /*
-   *
-   * Code goes here
-   *
-   */
+  const data = { user, text };
+  socket.emit("msg:post", data);
 }
 
 function render() {
